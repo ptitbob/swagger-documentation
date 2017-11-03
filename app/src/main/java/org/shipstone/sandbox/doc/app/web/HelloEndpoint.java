37427cfe14d2b11d2ad3068db9d3dc13706f0189
ptitbob <github@shipstone.org>
2017-11-03 21:17:07 +0100

    Ajout de la dependance Spring Swagger

--- app/src/main/java/org/shipstone/sandbox/doc/app/web/HelloEndpoint.java
+++ app/src/main/java/org/shipstone/sandbox/doc/app/web/HelloEndpoint.java
@@ -1,15 +1,19 @@
 package org.shipstone.sandbox.doc.app.web;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+
 /**
  * @author François Robert
  */
 @RestController
-@RequestMapping("hello")
+@RequestMapping(path = "hello", produces = {TEXT_PLAIN_VALUE})
+@Api
 public class HelloEndpoint {
 
   @GetMapping

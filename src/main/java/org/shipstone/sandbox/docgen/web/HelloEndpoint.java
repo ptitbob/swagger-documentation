aceb5a438f255627535493cc0aa5429982ac757b
ptitbob <github@shipstone.org>
2017-11-04 20:47:39 +0100

    Exposition super API REST :D

--- src/main/java/org/shipstone/sandbox/docgen/web/HelloEndpoint.java
+++ src/main/java/org/shipstone/sandbox/docgen/web/HelloEndpoint.java
@@ -1,7 +1,25 @@
 package org.shipstone.sandbox.docgen.web;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+
 /**
  * @author François Robert
  */
+@RestController
+@RequestMapping(value = "hello", produces = {TEXT_PLAIN_VALUE})
 public class HelloEndpoint {
+
+  @GetMapping
+  public String sayHello(
+      @RequestParam(name = "name", required = false, defaultValue = "stranger") String name
+  ) {
+    return "Hello " + name;
+  }
+
 }

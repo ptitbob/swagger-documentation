d1b3964614ae59a390070138a2b514bd2794536b
ptitbob <github@shipstone.org>
2017-11-04 20:42:01 +0100

    Ajout projet simple

---
+++ src/main/java/org/shipstone/sandbox/docgen/DocgenApplication.java
@@ -0,0 +1,12 @@
+package org.shipstone.sandbox.docgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocgenApplication.class, args);
+	}
+}
---
+++ src/test/java/org/shipstone/sandbox/docgen/DocgenApplicationTests.java
@@ -0,0 +1,16 @@
+package org.shipstone.sandbox.docgen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocgenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

2a54e232f29be83aa6147ca6a03f45c7c9aed2bb
ptitbob <github@shipstone.org>
2017-11-05 00:52:03 +0100

    optimisation des imports

--- src/test/java/org/shipstone/sandbox/docgen/web/HelloEndpointTest.java
+++ src/test/java/org/shipstone/sandbox/docgen/web/HelloEndpointTest.java
@@ -1,10 +1,8 @@
 package org.shipstone.sandbox.docgen.web;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
 /**
  * @author François Robert

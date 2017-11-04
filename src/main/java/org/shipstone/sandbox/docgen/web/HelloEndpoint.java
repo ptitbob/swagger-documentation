84cc39f71f97655cff487748562f7588087ce868
ptitbob <github@shipstone.org>
2017-11-04 22:52:05 +0100

    Ajout dependance pour la documentation SWAGGER et doc API exemple

--- src/main/java/org/shipstone/sandbox/docgen/web/HelloEndpoint.java
+++ src/main/java/org/shipstone/sandbox/docgen/web/HelloEndpoint.java
@@ -1,6 +1,7 @@
 package org.shipstone.sandbox.docgen.web;
 
-import org.springframework.http.MediaType;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +14,12 @@ import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
  */
 @RestController
 @RequestMapping(value = "hello", produces = {TEXT_PLAIN_VALUE})
+@Api
 public class HelloEndpoint {
 
+  @ApiOperation(
+      "Une API polie"
+  )
   @GetMapping
   public String sayHello(
       @RequestParam(name = "name", required = false, defaultValue = "stranger") String name
